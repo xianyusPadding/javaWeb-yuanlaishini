3d065113c3e0005c88959299a6158ddc2a53f80f
feilong <841583918@qq.com>
2017-03-26 14:28:13 +0800

    交友广场的侧栏

--- WebContent/js/makeFriend.js
+++ WebContent/js/makeFriend.js
@@ -1,5 +1,32 @@
-// $(function(){
-// 	$(".fl-navbar-left a").mouseover(function(){
-// 		$(".fl-navbar-left li").css("background",#DEDDD8);
-// 	})
-// })
+$(function(){
+	//让分组里的下拉列表和父元素的li等宽
+	$(".dropdown-menu").css("width",$(".dropdown").width()+"px");
+	
+	//index大于2的分组隐藏
+	$.each($(".fl-navbar-group>li"),function(index,value){
+		if(index>2)
+			$(value).css("display","none");
+	})
+	
+	//点击展开
+	$(".fl-group-openBtn").click(function(){
+		$.each($(".fl-navbar-group>li"),function(index,value){
+			if(index>2)
+				$(value).css("display","");
+		})
+		$(".fl-group-openBtn").css("display","none");
+		$(".fl-group-closeBtn").css("display","");
+	})
+	
+	//点击关闭
+	$(".fl-group-closeBtn").click(function(){
+		$.each($(".fl-navbar-group>li"),function(index,value){
+			if(index>2)
+				$(value).css("display","none");
+		})
+		$(".fl-group-closeBtn").css("display","none");
+		$(".fl-group-openBtn").css("display","");
+	})
+	
+	
+})
